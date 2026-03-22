f108da2391c8072af8a03e43dbd30730429b2344
Prahlad Bhakat <prahladbhakat05@gmail.com>
2026-03-22 23:43:30 +0530

    fix security root access

--- src/main/java/com/prahlad/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/prahlad/ecommerce/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
 				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+				.requestMatchers("/", "/actuator/health").permitAll()
 		        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .requestMatchers("/api/merchant/**").hasRole("MERCHANT")
                 .requestMatchers("/api/user/**").hasRole("USER")
